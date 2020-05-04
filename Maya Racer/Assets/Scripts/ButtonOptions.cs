3766954d24e8d98d0ab4f345f37b57c842a215b4
Maya A <fyy7680@autuni.ac.nz>
2020-05-04 15:19:04 +1200

    Uploaded Unity Project Files
    Uploaded existing project files for my game Maya's Racer

---
+++ Maya Racer/Assets/Scripts/ButtonOptions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOptions : MonoBehaviour { 
+
+    public void TrackSelect ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayGame () 
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void MainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SettingsMenu ()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    //Below here are track selection buttons
+    public void HillsideTrack ()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Maya Racer/Assets/Scripts/CarController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Dependency on input manager - script cannot function without this
+[RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    public InputManager im;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+
+    public float strengthCoefficient = 20000f;  //Torque
+    public float maxTurn = 20f;                 //Degrees
+    public Transform centreOfMass;              //Centre of mass
+    public Rigidbody rigidBody;
+
+    private void Start()
+    {
+        im = GetComponent<InputManager>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        if (centreOfMass)
+        {
+            rigidBody.centerOfMass = centreOfMass.localPosition;
+        }
+    }
+
+    //Meant for physics based process
+    private void FixedUpdate()
+    {
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
+        }
+
+        foreach (WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * im.steer;
+        }
+    }
+}
